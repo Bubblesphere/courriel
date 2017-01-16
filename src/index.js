b733d57d7969aea1e535d604f5acf76a33cded65
Bruno Hamel <Bruno Hamel>
2017-01-16 17:20:53 -0500

    template import des js creation de la page test application du layout sur courriel jade

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var path = require('path');
-var body_parse  = require('body-parser');
+var body_parse = require('body-parser');
 var json_file_object = require("json-file-object");
 var peers = require("./peers");
 
@@ -24,33 +24,37 @@ app.use('/img', express.static(path.join(__dirname, 'public/images')));
 app.use('/jss', express.static(path.join(__dirname, 'public/javascripts')));
 app.use('/css', express.static(path.join(__dirname, 'public/stylesheets')));
 
-app.get("/", function(req,res) {
+app.get("/", function (req, res) {
     res.render("courriel.jade");
 });
 
-app.get("/archive", function(req,res) {
+app.get("/archive", function (req, res) {
     res.render("courrielArchive.jade");
 });
 
-app.post("/storeEncryptedKey", body_parse.json(), function(req,res) {
+app.get("/test", function (req, res) {
+    res.render("test.jade");
+});
+
+app.post("/storeEncryptedKey", body_parse.json(), function (req, res) {
     console.log(req.body.encryptedKey);
     etat.encryptedKey = req.body.encryptedKey;
     res.json("ok");
 });
 
-app.post("/addAddress", body_parse.json(), function(req,res) {
+app.post("/addAddress", body_parse.json(), function (req, res) {
     etat.yp[req.body.pem] = req.body;
     res.json("Address added");
 });
 
 
-app.post("/postMessage", body_parse.json(), function(req,res) {
+app.post("/postMessage", body_parse.json(), function (req, res) {
     etat.letters.push(req.body);
     res.json("Message posted");
 });
 
 
-app.get("/etat", function(req,res) {
+app.get("/etat", function (req, res) {
     res.json(etat);
 });
 
