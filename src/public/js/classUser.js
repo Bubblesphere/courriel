0dfdf5897f5f0085c6f06e430f5c9854ada11f89
Deric Dallaire <dallaire.deric@gmail.com>
2017-01-17 12:39:12 -0500

    Removed old naming convention

--- src/public/js/classUser.js
+++
@@ -1,9 +0,0 @@
-function User (user) { 
-    var guid = new Guid();
-    this.objectType = "user",
-    this.id = guid.create(),
-    this.name = user.name,
-    this.update = function(newUser){
-        this.name = newUser.name;
-    }
-}
