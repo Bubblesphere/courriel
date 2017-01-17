06a65ab12fe0261a33d7916298cb586f13d9b3d2
Bruno Hamel <Bruno Hamel>
2017-01-16 21:58:48 -0500

    format du prototype du foreach

--- src/public/js/classMessage.js
+++ src/public/js/classMessage.js
@@ -1,7 +1,7 @@
-function Message (message) {
+function Message(message) {
     this.objectType = "message",
-    this.from = message.from,
-    this.title = message.tite,
-    this.receptionDate = new DateUtility(message.receptionDate),
-    this.content = message.content
+        this.from = message.from,
+        this.title = message.tite,
+        this.receptionDate = new DateUtility(message.receptionDate),
+        this.content = message.content
 }
--- src/public/js/frontEndCourriel.js
+++ src/public/js/frontEndCourriel.js
@@ -11,7 +11,7 @@ var user2 = new User({
 users.add(user1, user2);
 
 inbox.addMessage({
-    from:user2.id,
+    from: user2.id,
     title: "New message",
     receptionDate: new DateUtility('2015-12-12'),
     content: "hello dude"
@@ -19,14 +19,13 @@ inbox.addMessage({
 
 $(document).ready(function () {
     var messageHtml = $(".message-container:first").clone();
-    inbox.messages.forEach(function(message){
+    inbox.messages.forEach(function (message) {
         messageHtml.find(".title").html(message.title);
         messageHtml.find(".message").html(message.content);
         messageHtml.find(".date .fa-clock-o").html(message.receptionDate.getDaysElapsed());
-        console.log(users.getById(message.from));
         messageHtml.find(".user .fa-user").html(users.getById(message.from).name);
         $(".body").append(messageHtml);
     });
-    
+
 
 });
